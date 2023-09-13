241795bfa7f608439841d29398cb768478fba384
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-09-13 19:59:46 +0200

    home page done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
 import styles from './page.module.css';
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="https://images.pexels.com/photos/18278184/pexels-photo-18278184.jpeg" fill={true} alt='' />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDescription}>Handcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}></div>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,15 @@ import Hero from 'public/hero.png';
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.texts}>
-        <h1>Better desing for your digital products.</h1>
-        <p>Turning your idea into Reality. We bring together the teams from the global tech industry.</p>
-      </div>
-      <div className={styles.imgContainer}></div>
-      <Image src={Hero} width={50} height={50} alt='hero image' className={styles.img}/>
+    <div className={styles.container}>
+      <div className={styles.item}>
+          <h1 className={styles.title}>Better desing for your digital products.</h1>
+          <p className={styles.desc}>Turning your idea into Reality. We bring together the teams from the global tech industry.</p>
+          <button className={styles.button}>Dee Our Works</button>
+        </div>
+        <div className={styles.item}>
+        <Image src={Hero} className={styles.img} alt='hero image'/>
+      </div> 
     </div>
   )
 }
